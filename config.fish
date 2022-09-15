b29ada682229e04b905f81eb4ca3c5157cce5b7c
skyfire <skyfireitdiy@hotmail.com>
2022-09-15 23:46:16 +0800

    增加fish配置

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -x KUBECONFIG ~/.kube/config
+set -x PATH $PATH ~/.local/bin
+
+set -x PATH $PATH ~/.cargo/bin
--- install.sh
+++ install.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
-if [ -f ~/.vimrc ]; then
-	mv ~/.vimrc ~/.vimrc_bak
-	echo ~/.vimrc 被备份到 ~/.vimrc_bak
-fi
+config_vim(){
+	if [ -f ~/.vimrc ]; then
+		mv ~/.vimrc ~/.vimrc_bak
+	fi
 
-if [ -d ~/.vim ]; then
-	mv ~/.vim ~/.vim_bak
-	echo ~/.vim 被备份到 ~/.vim_bak
-fi
+	if [ -d ~/.vim ]; then
+		mv ~/.vim ~/.vim_bak
+	fi
 
-echo 安装 vim-plug ...
-# 使用 ghproxy 做代理
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://ghproxy.com/https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
+	# 使用 ghproxy 做代理
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		https://ghproxy.com/https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-sed -i 's/https:\/\/github\.com/https:\/\/ghproxy\.com\/https:\/\/github\.com/g' ~/.vim/autoload/plug.vim
+	sed -i 's/https:\/\/github\.com/https:\/\/ghproxy\.com\/https:\/\/github\.com/g' ~/.vim/autoload/plug.vim
 
-echo 创建软链接 $(pwd)/_vimrc -> ~/.vimrc
-ln -sf $(pwd)/_vimrc ~/.vimrc
+	ln -sf $(pwd)/_vimrc ~/.vimrc
 
-echo 安装成功！
-echo 首次启动 vim 的时候会有一些报错，因为相关插件未安装，需要执行命令 :PlugInstall
+	echo vim配置成功！
+	echo 首次启动 vim 的时候会有一些报错，因为相关插件未安装，需要执行命令 :PlugInstall
+}
+
+config_fish(){
+	mkdir -p ~/.config/fish
+	if [ -f ~/.config/fish/config.fish ]; then
+		mv ~/.config/fish/config.fish ~/.config/fish/config.fish_bak
+	fi
+	ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
+	if [ -f ~/.config/fish/fish_variables ]; then
+		mv ~/.config/fish/fish_variables ~/.config/fish/fish_variables_bak
+	fi
+	ln -sf $(pwd)/fish_variables ~/.config/fish/fish_variables
+	echo fish 配置成功
+}
+
+
+while test $# -gt 0
+do
+	config_$1
+	shift
+done
 
