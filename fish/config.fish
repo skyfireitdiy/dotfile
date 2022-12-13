2a5be487491330730e0ac6a988b39ceab4a71ab5
skyfire <skyfireitdiy@hotmail.com>
2022-12-13 23:34:40 +0800

    设置系统语言

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,8 @@ if status is-interactive
 end
 set -x KUBECONFIG ~/.kube/config
 set -x PATH $PATH ~/.local/bin
-
+set -x LANG C.UTF-8
 set -x PATH $PATH ~/.cargo/bin
+set -x PATH $PATH /opt/android-sdk/tools
 set --universal FZF_DEFAULT_COMMAND 'fd'
 
