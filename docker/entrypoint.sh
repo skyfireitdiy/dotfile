d3694855d9cd670ac2e1eee2e01da6385e8b732b
skyfire <skyfireitdiy@hotmail.com>
2023-07-17 23:14:47 +0800

    完成配置

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-install_vim(){
-    if [ ! -d ~/.config/nvim ]; then
-        cd /dotfile
+install_dotfile(){
+    if [ ! -d ~/.config/dotfile_installed ]; then
+        if [ ! -d ~/code ]; then
+            mkdir ~/code
+        fi
+        mkdir -p ~/.config/dotfile_installed
+        git clone https://ghproxy.com/https://github.com/skyfireitdiy/dotfile ~/code/dotfile
+        cd ~/code/dotfile
+        # 此处增加需要安装的软件
         ./install.sh vim
     fi
 }
 
-install_vim
+install_dotfile
 
-fish
+cd ~
+$@
