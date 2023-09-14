95e1438e32ddd36540a05b8ee08b59ebac76eda0
skyfire <skyfireitdiy@hotmail.com>
2023-09-14 22:37:23 +0800

    将命令移动到user中

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -x KUBECONFIG ~/.kube/config
-set -x PATH $PATH ~/.local/bin
-set -x LANG C.UTF-8
-set -x PATH $PATH ~/.cargo/bin
-set -x PATH $PATH /opt/android-sdk/tools
-set --universal FZF_DEFAULT_COMMAND 'fd'
 
+if test -f ~/.config/fish_user/config.fish
+    source ~/.config/fish_user/config.fish
+end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/anaconda/bin/conda
-    eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source
+    eval /opt/anaconda/bin/conda "shell.fish" hook $argv | source
 end
 # <<< conda initialize <<<
-
