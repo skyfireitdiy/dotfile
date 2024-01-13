ce92857b33a73bf4121b1cdaf2acc1ed6426f28d
skyfire <skyfireitdiy@hotmail.com>
2024-01-13 21:29:08 +0800

    将user中通用的配置移动到dotfile中

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,40 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
+# --------------------- abbr start ---------------------
+abbr -a -- np 'set -e http_proxy ; set -e https_proxy ; set -e socks_proxy '
+abbr -a -- e exit
+
+abbr -a -- rm 'rm -rf'
+
+abbr -a -- lg lazygit
+abbr -a -- a 'abbr -a'
+abbr -a -- um 'sudo reflector --verbose -c China --latest 10 --sort rate --save /etc/pacman.d/mirrorlist'
+abbr -a -- nv nvim
+abbr -a -- r ranger
+abbr -a -- s sudo
+abbr -a -- mk make
+abbr -a -- xm xmake
+abbr -a -- z zellij
+abbr -a -- vim nvim
+
+abbr -a -- gc 'git commit'
+abbr -a -- gs 'git status'
+abbr -a -- gco 'git checkout'
+abbr -a -- ga 'git add -u'
+abbr -a -- gpl 'git pull'
+abbr -a -- g git
+abbr -a -- gl 'git lg'
+abbr -a -- gps 'git push'
+
+abbr -a -- yi 'yay -S --noconfirm'
+abbr -a -- yu 'yay -Sy; yay -Su --noconfirm --needed --overwrite="*"'
+abbr -a -- ys 'yay -Ss'
+abbr -a -- yr 'yay -Rnsuvc'
+
+# --------------------- abbr end ---------------------
+
 if test -f ~/.config/fish_user/config.fish
     source ~/.config/fish_user/config.fish
 end
